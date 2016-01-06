2fd08a3556ba2bc79a885961f5d82adf493faadf
kylekatarnls <kylekatarnls@gmail.com>
2016-01-06 16:25:18 +0100

    Allow using PHPUnit to run tests

--- tests/phpunit.php
+++ tests/phpunit.php
@@ -5,12 +5,18 @@ require dirname(__FILE__) . '/../vendor/autoload.php';
 class JadePHPTest extends PHPUnit_Framework_TestCase {
 
     public function testCaseProvider() {
+<<<<<<< HEAD
         static $rawResults = null;
         if(is_null($rawResults)) {
             $rawResults = get_tests_results();
             $rawResults = $rawResults['results'];
         }
         return $rawResults;
+=======
+		chdir(dirname(__FILE__));
+		$rawResults = get_tests_results();
+		return $rawResults['results'];
+>>>>>>> 84ed821... Allow using PHPUnit to run tests
     }
 
     /**
