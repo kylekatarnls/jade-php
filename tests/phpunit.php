bf518e191530aab5639792ada0db376433d4bd05
kylekatarnls <kylekatarnls@gmail.com>
2016-01-07 14:00:45 +0100

    Get results only one

--- tests/phpunit.php
+++ tests/phpunit.php
@@ -5,8 +5,12 @@ require dirname(__FILE__) . '/../vendor/autoload.php';
 class JadePHPTest extends PHPUnit_Framework_TestCase {
 
     public function testCaseProvider() {
-		$rawResults = get_tests_results();
-		return $rawResults['results'];
+        static $rawResults = null;
+        if(is_null($rawResults)) {
+            $rawResults = get_tests_results();
+            $rawResults = $rawResults['results'];
+        }
+        return $rawResults;
     }
 
     /**
